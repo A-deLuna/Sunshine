55f04026d5bf3a871b65a1a56e33019d33d919ad
OneKorg <tonydelun@gmail.com>
2014-08-12 22:44:27 -0700

    shareActivity thing done :)

--- app/src/main/java/com/example/admin/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/admin/sunshine/DetailActivity.java
@@ -3,7 +3,9 @@ package com.example.admin.sunshine;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,7 +14,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class DetailActivity extends ActionBarActivity {
-    private String detailedForecast = "";
+    private ShareActionProvider mShareActionProvider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +31,16 @@ public class DetailActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.detail, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,this.getIntent().getStringExtra(Intent.EXTRA_TEXT)
+                + " #SunshineApp" );
+        mShareActionProvider.setShareIntent(intent);
+
+
         return true;
     }
 
@@ -41,6 +53,8 @@ public class DetailActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             Intent intent = new Intent(this,SettingsActivity.class);
             startActivity(intent);
+
+
             return true;
         }
         if(id == R.id.action_share) {
@@ -66,10 +80,10 @@ public class DetailActivity extends ActionBarActivity {
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
             TextView tv = (TextView)rootView.findViewById(R.id.detail_textview);
 
-            if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT))
-                detailedForecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+            if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+                String detailedForecast = intent.getStringExtra(Intent.EXTRA_TEXT);
                 tv.setText(detailedForecast);
-
+            }
             return rootView;
         }
     }
--- app/src/main/java/com/example/admin/sunshine/MainActivity.java
+++ app/src/main/java/com/example/admin/sunshine/MainActivity.java
@@ -13,11 +13,12 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
-    private final String TAG = "MainActivity";
+    private static final String TAG = "MainActivity";
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate()");
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
@@ -26,6 +27,35 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart()");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
